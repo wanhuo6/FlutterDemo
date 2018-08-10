90e15cf6cc3596e42bd7c1891c22bf8b576bb4a1
liuhuijie <liuhuijie@kuaikuaikeji.com>
2018-08-10 18:39:45 +0800

    添加底部导航

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,79 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MyHomePage());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  MyHomePageState createState() => new MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MyHomePageState extends State<MyHomePage> {
+  final tabTextStyleNormal = new TextStyle(color: const Color(0xff515151));
+  final tabTextStyleSelected = new TextStyle(color: const Color(0xff1296db));
+  int tabIndex;
+  var tabImages;
+  var appBarTitles;
+  Image getTabImage(path) {
+    return new Image.asset(path, width: 20.0, height: 20.0);
   }
-
+  Image getTabIcon(int curIndex) {
+    if (curIndex == tabIndex) {
+      return tabImages[curIndex][1];
+    }
+    return tabImages[curIndex][0];
+  }
+  void initData() {
+    tabIndex=0;
+    appBarTitles = ['主页', '我的'];
+      tabImages =
+      [
+        [getTabImage('images/ic_home_page_normal.png'), getTabImage('images/ic_home_page_select.png')],
+        [getTabImage('images/ic_mine_normal.png'), getTabImage('images/ic_mine_select.png')],
+      ];
+  }
+    TextStyle getTabTextStyle(int curIndex) {
+      if (curIndex == tabIndex) {
+        return tabTextStyleSelected;
+      }
+      return tabTextStyleNormal;
+    }
+    Text getTabTitle(int curIndex) {
+      return new Text(appBarTitles[curIndex], style: getTabTextStyle(curIndex));
+    }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+    initData();
+    return new MaterialApp(
+      theme: new ThemeData(
+          primaryColor: const Color(0xFF63CA6C)
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+      home: new Scaffold(
+        appBar: new AppBar(
+            title: new Text(appBarTitles[tabIndex],
+                style: new TextStyle(color: Colors.white)),
+            iconTheme: new IconThemeData(color: Colors.white)
+        ),
+        bottomNavigationBar: new CupertinoTabBar(
+          items: <BottomNavigationBarItem>[
+            new BottomNavigationBarItem(
+                icon: getTabIcon(0),
+                title: getTabTitle(0)),
+            new BottomNavigationBarItem(
+                icon: getTabIcon(1),
+                title: getTabTitle(1)),
           ],
+          currentIndex: tabIndex,
+          onTap: (index) {
+            setState(() {
+              tabIndex = index;
+            });
+          },
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
 }
